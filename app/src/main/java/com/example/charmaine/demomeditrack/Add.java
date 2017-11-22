e28397ea7600baa8232ffb382755d54ac03c304f
charmainegohsm <charmainegohsm@users.noreply.github.com>
2017-11-22 12:06:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/charmaine/demomeditrack/Add.java
@@ -0,0 +1,8 @@
+package com.example.charmaine.demomeditrack;
+
+/**
+ * Created by Charmaine on 21/11/2017.
+ */
+
+class Add {
+}
---
+++ app/src/main/java/com/example/charmaine/demomeditrack/Add2.java
@@ -0,0 +1,13 @@
+package com.example.charmaine.demomeditrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add2);
+    }
+}
